ca70fe52c70da5985a4ef3e00b4fc9b7e9dd1c8d
Dimelena <3apy@mail.ru>
2023-05-04 23:53:34 +0300

    Added program file

---
+++ HW_CS_02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Homework 02
+
+// Task 1
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа. Через строку решать нельзя.
+
+int Request(string req)
+{
+    Console.Write(req);
+    int resalt = Convert.ToInt32(Console.ReadLine());
+    return resalt;
+}
+
+int num = Request("Input a three-digit number: ");
+if (num < 100 || num > 1000)
+{
+    Console.WriteLine("Repeat the entry (Your number is not a three-digit number).");
+    return;
+} 
+int SecondDigit = num / 10 % 10;
+Console.WriteLine("Second digit of number is " + SecondDigit);
