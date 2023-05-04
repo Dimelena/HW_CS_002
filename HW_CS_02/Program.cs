7d81ae7a917452807f0ca749d0b0171bffed0269
Dimelena <3apy@mail.ru>
2023-05-04 23:59:56 +0300

    Added Task 2

--- HW_CS_02/Program.cs
+++ HW_CS_02/Program.cs
@@ -4,6 +4,7 @@
 // Напишите программу, которая принимает на вход трёхзначное число и на выходе 
 // показывает вторую цифру этого числа. Через строку решать нельзя.
 
+/*
 int Request(string req)
 {
     Console.Write(req);
@@ -19,3 +20,40 @@ if (num < 100 || num > 1000)
 } 
 int SecondDigit = num / 10 % 10;
 Console.WriteLine("Second digit of number is " + SecondDigit);
+*/
+
+
+// Task 13
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.Через строку решать нельзя.
+
+/*
+int Request(string req)
+{
+    Console.Write(req);
+    int resalt = Convert.ToInt32(Console.ReadLine());
+    return resalt;
+}
+int CutNumber(int num)
+{
+    while (num > 1000)
+    {
+        num /=10;
+    }
+    return num % 10;
+}
+bool Verify(int num)
+{
+    if (num < 100)
+    {
+        Console.WriteLine("There is no third digit");
+        return false;
+    }
+    return true;
+}
+int num = Request("Input a number ");
+if (Verify(num))
+{
+    Console.WriteLine(CutNumber(num));
+} 
+*/
